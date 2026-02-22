3f05678eed2f7ff9025fe490d1371f3bb4393a85
yangyanghyunjung <hyunjungyang0922@gmail.com>
2026-02-22 18:13:42 +0900

    feat: add yaml file

--- src/main/java/com/lotto/LottoApplication.java
+++ src/main/java/com/lotto/LottoApplication.java
@@ -1,5 +1,6 @@
 package com.lotto;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LottoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(LottoApplication.class, args);
 	}
 }
